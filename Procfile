61d101d1c8dfb0e2b426e5085b3215ec386a025a
Charles Montama <c.montama.l@gmail.com>
2021-02-17 17:06:26 +0000

    add streamlit app after modifying 'Procfile'

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn app:app
+#web: gunicorn app:app                                                           #standard use do not change
+web: sh setup.sh && streamlit run app_streamlit.py
--- ui_streamlit.py
+++ ui_streamlit.py
@@ -1,6 +1,10 @@
 import streamlit as st
+# add all the necessary functions import
+
+st.markdown("# ML Chartist")
+st.markdown("**NASDAQ stocks prediction (attempt)**")
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #df = read_data()
     #main()
