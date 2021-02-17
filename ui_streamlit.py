63ecad36c2005c9c3dade3362c45b99c3a614419
Charles Montama <c.montama.l@gmail.com>
2021-02-17 19:05:56 +0000

    set up blank streamlit interface and blank api

--- ui_streamlit.py
+++ ui_streamlit.py
@@ -2,7 +2,21 @@ import streamlit as st
 # add all the necessary functions import
 
 st.markdown("# ML Chartist")
-st.markdown("**NASDAQ stocks prediction (attempt)**")
+st.markdown("**🚀 NASDAQ stocks prediction (attempt) 🚀**")
+
+st.markdown("""ML Chartist gives you the opportunity to run backtest to measure
+the effectiveness of investment strategies of our""")
+
+
+# Ask what stock(s) user want to invest in
+    # Show most likely return
+    # Show graph
+
+
+# Evaluate probality of positive returns for previously hold stocks to
+    # Ask stocks
+    # Ask price and date user bought it
+
 
 
 #if __name__ == "__main__":
