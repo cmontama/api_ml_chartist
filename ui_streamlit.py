04c30d53f81a00f60289224780f012167672b108
Charles Montama <c.montama.l@gmail.com>
2021-02-18 22:06:22 +0000

    pursuing `main()` implementation to check the output: improving stock number selection warning

--- ui_streamlit.py
+++ ui_streamlit.py
@@ -12,41 +12,41 @@ st.markdown("""Our (amazing, super-effective and transcendental) model gives
 
 st.markdown("## Create a portfolio based on the NASDAQ100 companies")
 
-
-# Ask how many stocks user want to invest in
-    # Restric to [1-10]
-    # Warn that a too low number (1,2) is unlikely to give the best returns
-    # Warn that a too high number (7+) is unlikely to give better returns than average
-    # Show graph
-
 n_stock = st.slider('Select the number of companies', 1, 10, 5)
 
 
 def main():
-    """Ask how many stocks user want to invest in
-    Restricts to [1-10]
-    Warn that a too low number (1,2) is unlikely to give the best returns
-    Warn that a too high number (7+) is unlikely to give better returns than average
-    Show graph"""
+    """
+    -Ask how many stocks user want to invest in
+    -Restricts to [1-10]
+    -Warn that a too low number (1,2) is unlikely to give the best returns
+    -Warn that a too high number (7+) is unlikely to give better returns than average
+    -Show graph
+
+    """
+
     print(f"User selected {n_stock} stocks for his portfolio")
     if n_stock <= 3:
         st.markdown("⚠️⚠️⚠️")
         st.markdown("The selected portfolio contains too few stocks")
         st.markdown("""Research have shown you should add more stock in your
             portfolio in order to get a better return""")
+        st.markdown("⚠️⚠️⚠️")
+
+
+    # Improvement idea: run the integral of the portfolio return against the
+    #   index one to increase robustness of stock number selection
 
     if n_stock >= 7:
         st.markdown("⚠️⚠️⚠️")
-        st.markdown("The selected portfolio contains too few stocks")
-        st.markdown("""Research have shown you should add more stock in your
-            portfolio in order to get a better return""")
+        st.markdown("The selected portfolio contains too many stocks")
+        st.markdown("""In order to avoid a unoptimal return (too close from the
+            average return, i.e the NASDAQ100 one) please select a lower number
+            of companies""")
+        st.markdown("⚠️⚠️⚠️")
 
 
 
-
-# Evaluate probality of positive returns for previously hold stocks to
-    # Ask stocks
-    # Ask price and date user bought it
 
 
 
