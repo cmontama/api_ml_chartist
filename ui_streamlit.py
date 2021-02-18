51853a06e8b512d1b27fd0bc4bbed031b591e025
Charles Montama <c.montama.l@gmail.com>
2021-02-18 21:49:13 +0000

    begin `main()` implementation to check the output

--- ui_streamlit.py
+++ ui_streamlit.py
@@ -7,9 +7,10 @@ st.markdown("**🚀 NASDAQ stocks prediction (attempt) 🚀**")
 st.markdown("""ML Chartist gives you the opportunity to run backtest to measure
 the effectiveness of investment strategies of our""")
 
-st.markdown("Our (amazing, super-effective and transcendental) model gives **_you_** the stock the most likely to have the best positive returns")
+st.markdown("""Our (amazing, super-effective and transcendental) model gives
+    ***you*** the stock the most likely to have the best positive returns""")
 
-st.markdown("")
+st.markdown("## Create a portfolio based on the NASDAQ100 companies")
 
 
 # Ask how many stocks user want to invest in
@@ -18,6 +19,29 @@ st.markdown("")
     # Warn that a too high number (7+) is unlikely to give better returns than average
     # Show graph
 
+@st.cache
+def main():
+    """Ask how many stocks user want to invest in
+    Restricts to [1-10]
+    Warn that a too low number (1,2) is unlikely to give the best returns
+    Warn that a too high number (7+) is unlikely to give better returns than average
+    Show graph"""
+    n_stock = st.slider('Select the number of companies', 1, 10, 5)
+    print(f"User selected {n_stock} stocks for his portfolio")
+    if n_stock <= 3:
+        st.markdown("⚠️⚠️⚠️")
+        st.markdown("The selected portfolio contains too few stocks")
+        st.markdown("""Research have shown you should add more stock in your
+            portfolio in order to get a better return""")
+
+    if n_stock >= 7:
+        st.markdown("⚠️⚠️⚠️")
+        st.markdown("The selected portfolio contains too few stocks")
+        st.markdown("""Research have shown you should add more stock in your
+            portfolio in order to get a better return""")
+
+
+
 
 # Evaluate probality of positive returns for previously hold stocks to
     # Ask stocks
@@ -25,6 +49,5 @@ st.markdown("")
 
 
 
-#if __name__ == "__main__":
-    #df = read_data()
-    #main()
+if __name__ == "__main__":
+    main()
